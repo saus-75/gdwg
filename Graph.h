1a56926a1456a35f717f4fdd722e721422e45427
saus-75 <georgechieng@gmail.com>
2017-09-22 06:04:19 +1000

    code refactoring successful, 4more function to go and 4 more mini func left, end my suffering

--- Graph.h
+++ Graph.h
@@ -32,18 +32,18 @@ namespace gdwg {
 
         bool addNode(const N& val);
         bool addEdge(const N& src, const N& dst, const E& w);
-        // bool replace(const N& oldData, const N& newData);
+        bool replace(const N& oldData, const N& newData);
         // void mergeReplace(const N& oldData, const N& newData);
-        // void deleteNode(const N&) noexcept;
+        void deleteNode(const N& val) noexcept;
         void deleteEdge(const N& src, const N& dst, const E& w) noexcept;
         void clear() noexcept;
         bool isNode(const N& val) const;
         bool isConnected(const N& src, const N& dst) const;
 
-        //TODO sort by ascending out-degrees
         void printNodes() const;
-        //TODO sort by ascending weights
         void printEdges(const N& val) const;
+        //REMOVE AT THE END!!
+        void printInEdges(const N& val) const;
 
         // void begin() const {}
         // bool end() const {}
@@ -53,7 +53,9 @@ namespace gdwg {
 
     private:
         class Node;
-        std::vector<Node> nodes;
+        using Node_ = std::shared_ptr<Node>;
+
+        std::vector<Node_> nodes;
 
     private:
         class Node {
@@ -63,14 +65,21 @@ namespace gdwg {
 
             ~Node(){}
 
-            bool formEdge(Node& dst, const E& weight);
+            void replace(const N& newData){nodePtr = std::make_shared<N>(newData);}
+            bool formEdge(Node_& dst, const E& weight);
+            void formInEdge(Node_& src, const E& weight);
             bool removeEdge(const N& dst, const E& w) noexcept;
-            bool isEdge(const Node& dst, const E& weight) const;
+            void removeInEdge(const N& src, const E& w) noexcept;
+            bool isEdge(const Node_& dst, const E& weight) const;
             bool isConnected(const Node& dst) const;
-            void clearEdges(){edges.clear();}
-            void printEdges() const;
-            void incIn(){in_++;}
-            void decIn(){in_--;}
+            void cleanEdges();
+            void clearEdges(){inEdges.clear(); edges.clear();}
+            void printEdges();
+            void printInEdges();
+
+            void upIn(){in_ = inEdges.size();}
+            void upOut(){out_ = edges.size();}
+            void upAll(){in_ = inEdges.size(); out_ = edges.size();}
 
             std::weak_ptr<N> getNodePtr() const {return nodePtr;}
             const N& getNode() const {return *nodePtr;}
@@ -80,29 +89,34 @@ namespace gdwg {
         private:
             //Forward declaration of Edge class
             class Edge;
+            using Edge_ = std::shared_ptr<Edge>;
+
             //a shared ptr pointing to a val
             std::shared_ptr<N> nodePtr;
             //number of incoming edges
-            unsigned int in_;
+            int in_;
             //number of outgoing edges
-            unsigned int out_;
-            //comprises all nodes its connected to
-            std::vector<Edge> edges;
+            int out_;
+            //comprises all outgoing nodes
+            std::vector<Edge_> edges;
+            //comprises all incoming nodes
+            std::vector<Edge_> inEdges;
 
         private:
             class Edge {
             public:
-                Edge(const Node& dst, const E& w) : 
-                    weight_{w}, dst_{dst.getNodePtr()}{}
+                Edge(const Node_& dst, const E& w) : 
+                    weight_{std::make_shared<E>(w)}, dst_{dst}{}
                 
                 ~Edge(){}
-
-                const N& getDst() const { auto dst = dst_.lock(); return *dst; }
-                const E& getWeight() const { return weight_; }
+                
+                const bool canUnlock() const { if(auto dst = dst_.lock()){ return true; } return false; } 
+                const Node& getDst() const { auto dst = dst_.lock(); return *dst; }
+                const E& getWeight() const { return *weight_; }
 
             private:
-                E weight_;
-                std::weak_ptr<N> dst_;
+                std::shared_ptr<E> weight_;
+                std::weak_ptr<Node> dst_;
             };
         };
     };
