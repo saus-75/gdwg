f263b840371edb4cbe1caad936cd40046e958f34
saus-75 <georgechieng@gmail.com>
2017-09-22 16:40:36 +1000

    most of the function works, mergeReplace partially work, no time

--- Graph.h
+++ Graph.h
@@ -13,6 +13,9 @@ namespace gdwg {
     template <typename N, typename E>
     class Graph {
     
+    private:
+        class Node;
+        using Node_ = std::shared_ptr<Node>;
     public:
         Graph(){
             std::cout << "[ Graph Created ]\n";
@@ -22,58 +25,94 @@ namespace gdwg {
             std::cout << "[ Graph Destroyed ]\n"; 
         };
 
-        //copy and move
-        // Graph(const Graph& b);
-        // Graph(Graph&& b);
+        Graph(const Graph& b){
+            for (auto i : b.getNodes()){
+                nodes.push_back(std::make_shared<Node>(Node(*i)));
+            }
+        }
 
-        //copy and move assignment
-        // Graph& operator= (const Graph& b);
-        // Graph& operator= (Graph&& b);
+        Graph(Graph&& b){
+            nodes = std::move(b.nodes);
+        }
+
+        Graph& operator= (const Graph& b){
+            clear();
+            for (auto i : b.getNodes()){
+                nodes.push_back(std::make_shared<Node>(Node(*i)));
+            }
+            return this;
+        }
+        Graph& operator= (Graph&& b){
+            clear();
+            nodes = std::move(b.nodes);
+            return this;
+        }
 
         bool addNode(const N& val);
         bool addEdge(const N& src, const N& dst, const E& w);
         bool replace(const N& oldData, const N& newData);
-        // void mergeReplace(const N& oldData, const N& newData);
+        void mergeReplace(const N& oldData, const N& newData);
         void deleteNode(const N& val) noexcept;
         void deleteEdge(const N& src, const N& dst, const E& w) noexcept;
         void clear() noexcept;
         bool isNode(const N& val) const;
         bool isConnected(const N& src, const N& dst) const;
+        const std::vector<Node_> getNodes() const {return nodes;}
+
 
         void printNodes() const;
         void printEdges(const N& val) const;
         //REMOVE AT THE END!!
         void printInEdges(const N& val) const;
-
-        // void begin() const {}
-        // bool end() const {}
-        // void next() const {nodes.next();}
-        // const N& value() const {nodes.begin()->getNode();}
+        
+        void begin() const {itr = 0;}
+        bool end() const {if (itr == nodes.size()) return true; return false;}
+        void next() const {itr++;}
+        const N& value() const {return (*nodes[itr]).getNode();}
         //TODO remove debugging prints
 
     private:
-        class Node;
-        using Node_ = std::shared_ptr<Node>;
 
         std::vector<Node_> nodes;
+        mutable unsigned int itr = 0;
 
     private:
         class Node {
+        private:
+            //Forward declaration of Edge class
+            class Edge;
+            using Edge_ = std::shared_ptr<Edge>;
+
         public:
             Node(const N& val) : 
                 nodePtr{std::make_shared<N>(val)}, in_{0}, out_{0}{}
 
             ~Node(){}
 
+            Node(const Node& b): 
+                in_{b.getIn()}, out_{b.getOut()}, nodePtr{std::make_shared<N>(b.getNode())} 
+            {
+                for (auto i : b.getOutEdge()){
+                    edges.push_back(std::make_shared<Edge>(Edge(*i)));
+                }
+                for (auto i : b.getInEdge()){
+                    edges.push_back(std::make_shared<Edge>(Edge(*i)));
+                }
+            }
+
             void replace(const N& newData){nodePtr = std::make_shared<N>(newData);}
+            
             bool formEdge(Node_& dst, const E& weight);
             void formInEdge(Node_& src, const E& weight);
             bool removeEdge(const N& dst, const E& w) noexcept;
             void removeInEdge(const N& src, const E& w) noexcept;
+            
             bool isEdge(const Node_& dst, const E& weight) const;
             bool isConnected(const Node& dst) const;
+            
             void cleanEdges();
             void clearEdges(){inEdges.clear(); edges.clear();}
+            
             void printEdges();
             void printInEdges();
 
@@ -83,23 +122,16 @@ namespace gdwg {
 
             std::weak_ptr<N> getNodePtr() const {return nodePtr;}
             const N& getNode() const {return *nodePtr;}
-            const unsigned int getIn() const {return in_;}
-            const unsigned int getOut() const {return out_;}
+            const int getIn() const {return in_;}
+            const int getOut() const {return out_;}
+            const std::vector<Edge_> getOutEdge() const {return edges;}
+            const std::vector<Edge_> getInEdge() const {return inEdges;}
         
         private:
-            //Forward declaration of Edge class
-            class Edge;
-            using Edge_ = std::shared_ptr<Edge>;
-
-            //a shared ptr pointing to a val
             std::shared_ptr<N> nodePtr;
-            //number of incoming edges
             int in_;
-            //number of outgoing edges
             int out_;
-            //comprises all outgoing nodes
             std::vector<Edge_> edges;
-            //comprises all incoming nodes
             std::vector<Edge_> inEdges;
 
         private:
@@ -110,8 +142,11 @@ namespace gdwg {
                 
                 ~Edge(){}
                 
+                Edge(const Edge& b): weight_{std::make_shared<E>(b.getWeight())}, dst_{b.getDstPtr()}{}
+
                 const bool canUnlock() const { if(auto dst = dst_.lock()){ return true; } return false; } 
                 const Node& getDst() const { auto dst = dst_.lock(); return *dst; }
+                const std::weak_ptr<Node> getDstPtr() const { auto dst = dst_.lock(); return dst; }
                 const E& getWeight() const { return *weight_; }
 
             private:
